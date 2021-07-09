dee4904b7eb1ed061af8bbcdefb8f5d473126d28
Abhishek_Roy1 <abhishek2097@gmail.com>
2021-07-09 20:30:29 +0530

    initial commit

---
+++ src/test/java/com/spring/config/server/Spring/Config/Server/Demo/SpringConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.config.server.Spring.Config.Server.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
